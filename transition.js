2a8dcd53d2c7c51129ecb59ee777d87f28fd5fca
Carlos Lucar <lucar.carlos1@gmail.com>
2021-05-12 17:41:01 -0400

    updated

--- transition.js
+++ transition.js
@@ -1,19 +1,21 @@
 window.onload =() =>{
     const transition_el = document.querySelector('.transition');
-    const anchors = document.querySelectorAll('a');
+    const anchors = document.querySelectorAll('.navbar-brand');
 
     setTimeout(() =>{
-        transition_el.classList.remove('.is_active');
+        transition_el.classList.remove('is-active');
     },500);
 
     for (let i=0; i < anchors.length; i++){
         const anchor = anchors[i];
 
-        anchor.addEventListener('click',e=>{
+        anchor.addEventListener('click',e => {
             e.preventDefault();
             let target = e.target.href;
 
-            transition_el.classList.add('is_active');
+            console.log(target);
+
+            transition_el.classList.add('is-active');
 
             setTimeout(()=>{
                 window.location.href = target;
